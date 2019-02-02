b4b5a263ef55a80c49182c39630e48dce448fd75
Josh Smith <jesmith17@gmail.com>
2019-02-01 20:36:46 -0600

    Adding default user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Calendar.create(title: 'Test Event', description: 'Testing that I can create events correctly', start_date: DateTime.now, end_date: DateTime.now + 6.hours, url: nil);
+User.create(
+    email: "jesmith17@gmail.com",
+    password: "Titleist",
+    password_confirmation: 'Titleist'
+)
